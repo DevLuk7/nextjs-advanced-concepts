aa3d3657d45c92896c6c8cdb18e5dd6260673d3b
DevLuk7 <lukass100@gmail.com>
2024-08-16 13:24:53 +0200

    add circuit breaker

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  RequestTimeoutException,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { CircuitBreakerInterceptor } from 'src/common/interceptors/circuit-breaker.interceptor';
 
+@UseInterceptors(CircuitBreakerInterceptor)
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
@@ -14,7 +26,9 @@ export class CoffeesController {
 
   @Get()
   findAll() {
-    return this.coffeesService.findAll();
+    // return this.coffeesService.findAll();
+    console.log('🦊 "findAll" executed');
+    throw new RequestTimeoutException('Error');
   }
 
   @Get(':id')
